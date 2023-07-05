fd515c61c8c2997c132f307368b412d935736e2d
shawnbirring <shawnbirring21@gmail.com>
2023-07-05 14:25:46 -0700

    example

--- ping.py
+++ ping.py
@@ -1,23 +1,11 @@
-import os
 import subprocess
 
 classrooms = [
-    "NE01L0143",
-    "NE01L0201",
-    "NE01L0245",
-    "NE01L0246A",
-    "NE01L0334",
-    "NE01L0338",
-    "NE01L0340",
-    "NE10L0108",
-    "NE12L0201",
-    "NE18L0106",
-    "NE20L0221",
-    "SE12L0318",
-    "SE12L0321",
-    "SE12L0322",
-    "SE12L0324",
+    "SE10L0102",
+    "SE10L0130",
+    "SE10L0131",
 ]
+
 
 for classroom in classrooms:
     for i in range(31):  # Adjust range as per your requirements
